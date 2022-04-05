05dca572ab9f6c2f867e6774e6e828e1a5a385b7
Karl <24803299+KaRob121@users.noreply.github.com>
2022-04-05 15:59:18 -0700

    Completed page by fixing bullet point styling and adding a background image

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,7 +2,7 @@ function Main() {
     return (
         <main>
             <h1 className="main--heading">Fun facts about React</h1>
-            <ul>
+            <ul className="main--facts">
                 <li>Was first released in 2013</li>
                 <li>Was originally created by Jordan Walke</li>
                 <li>Has well over 100K stars on GitHub</li>
