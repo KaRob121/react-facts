68c56014f0ab4ddbe13eec34fc793e7d52df96bc
Karl <24803299+KaRob121@users.noreply.github.com>
2022-05-03 18:46:25 -0700

    Added working dark mode functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
+import React from 'react'
 import Main from './components/Main'
 import Navbar from './components/Navbar';
 import './App.css';
 
 function App() {
+  const [darkMode, setDarkMode] = React.useState(false)
+
+  function toggleDarkMode() {
+    setDarkMode(prevDarkMode => !prevDarkMode)
+  }
+
   return (
     <div className="container">
-      <Navbar darkMode=""/>
-      <Main darkMode=""/>
+      <Navbar 
+        darkMode={ darkMode }
+        toggleDarkMode={ toggleDarkMode }
+      />
+      <Main 
+        darkMode={ darkMode }
+      />
     </div>
   )
 }
